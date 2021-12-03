9b56f1aadd6a1d74f47a74d6ba0e5e3c855e3a93
Le Hung <ktr.lehung@gmail.com>
2021-12-03 13:02:54 +0700

    assignment 1

---
+++ Assignment 1/bt 2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	float a,b,c,d;
+	printf("Nhap a=");
+	scanf("%f",&a);
+	printf("Nhap b=");
+	scanf("%f",&b);
+	printf("Nhap c=");
+	scanf("%f",&c);
+	d=b*b-4*a*c;
+	if(d>0){
+		printf("pt co 2 nghiem x1 = -b+sqrt(d/a)");
+	}else{
+	}
+	}
+}
